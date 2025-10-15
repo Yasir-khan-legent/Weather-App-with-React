7255487fa060a9919bae9d7dc910f13fa121e16f
Yasir-khan <techpro54321@gmail.com>
2025-10-14 22:04:17 -0700

    completed project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Ui from './componnet/function';
+import './componnet/function.css'
+
+
+function App() {
+
+  return (
+    <div>
+  <Ui/>
+ 
+    </div>
+  );
+}
+
+export default App
